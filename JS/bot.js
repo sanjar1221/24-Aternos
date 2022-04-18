f2da083eda418399ffed130b9e747957eb25c1d4
sanjar1221 <103960998+sanjar1221@users.noreply.github.com>
2022-04-19 01:08:25 +0600

    Update bot.js

--- JS/bot.js
+++ JS/bot.js
@@ -4,8 +4,8 @@ console.log('Starting...')
 
 function createBot () {
     const bot = mineflayer.createBot({
-    host: "localhost",
-    port: "25565",
+    host: "VioSnip.aternos.me",
+    port: "42766",
     username: "Test",
     version: false
     })
